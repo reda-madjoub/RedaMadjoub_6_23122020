98629b5dc6769e01f0138bcdd6dba9e70aced7b4
Reda M <reda.madjoub@gmail.com>
2021-01-09 15:04:30 +0100

    trap tabulation inside modal for accessibility

--- src/js/main.js
+++ src/js/main.js
@@ -119,30 +119,28 @@ tagFilter.forEach(item => item.addEventListener("click",(e) => {
     filterByTags(e.target.innerText.substring(1).toLowerCase())
 }))
 
-/* debut test  ==> event delegation*/
+// EVENT DELEGATION ==> ajout d'un event pour une element n'existant pas encore 
+// Il sera rajouter dynamiquement
 cards[0].addEventListener("click",(e) => {
-    // console.log(e.target.classList.value === 'tag-link' ||e.target.classList.value === 'tag');
-    // console.log(e.target);
-    // console.log(e.target.parentNode);
-
-    
-    //****** */
     // click sur tag sous la photo d'un photographe
     if(e.target.classList.value === 'tag') { 
         const tagPhotographe = e.target.innerText.substring(1).toLowerCase()
-        
         // Tag sous la photo d'un photographe deja selectionné ? (verification dans le tab)
         if(tab.includes(tagPhotographe) ) {
             tagFilter.forEach(item => {
+                // le tag a deja etait selectionné
                 if(item.innerText.substring(1).toLowerCase() === tagPhotographe) {
                     if(item.classList.contains("selected")) {
+                        // suppression de la class "selected"
                         item.classList.remove("selected")
                     }else {
+                        // ajout de la class "selected"
                         item.classList.add("selected")
                     }
                 }
             })
         }else {
+            // le tag n'a jamais était selectionné
             tagFilter.forEach(item => {
                 if(item.innerText.substring(1).toLowerCase() === tagPhotographe) {
                     if(item.classList.contains("selected")) {
@@ -157,122 +155,5 @@ cards[0].addEventListener("click",(e) => {
     }
 })
 
-
-
-
-
-
-
-/* fin test */
-
-
-
-
 fetchPhotographers(data)
 fetchTags(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import './data.js'
-
-// // DOM ELEMENTS
-// const cards = document.getElementsByClassName("cards")
-// const tags = document.getElementsByClassName("tags")
-// const tagFilter = document.querySelectorAll("nav a")
-
-
-// // RESQUEST
-// const requestOptions = {
-//     method: 'GET',
-//     redirect: 'follow'
-// };
-// const requete = async() => {
-//     const response = await fetch("https://cors-anywhere.herokuapp.com/https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P5+Javascript+%26+Accessibility/FishEyeDataFR.json", requestOptions)
-//     const data = await response.json()
-//     fetchPhotographers(data)
-//     fetchTags(data)
-// } 
-
-// // AFFICHER LES PHOTOGRAPHES
-// const fetchPhotographers = async(data) => {
-//     let result =''
-//     const resultat = await [...data.photographers].map(item => {
-//         return item
-//     })
-//     for(let i = 0; i < resultat.length; i++) {
-//         result += `
-//         <div class="card">
-//             <a href="#">
-//                 <img src="https://res.cloudinary.com/dps3eww2i/image/upload/v1609005020/P6-img/${resultat[i].portrait}" alt="${resultat[i].name}">
-//                 <h2>${resultat[i].name}</h2>
-//             </a>
-//             <p>${resultat[i].city}, ${resultat[i].country}</p>
-//             <p>${resultat[i].tagline}</p>
-//             <p>${resultat[i].price}€/jour</p>
-//             <div class="tags"></div>
-//         </div>
-//         `
-//     }
-//             cards[0].innerHTML = result;
-//             return resultat;
-// }
-
-//     // AFFICHER LES TAGS SOUS CHAQUE PHOTOGRAPHES
-//     const fetchTags = async(data) => {
-//         let affichageTags =''
-//         const res = await[...data.photographers].map(item => {
-//             console.log(item.tags);
-//             return item.tags
-//         })
-//         for(let i = 0; i < res.length; i++) {
-//             for(let j = 0; j < res[i].length; j++) {
-//                 affichageTags += `
-//                     <a class="tag-link" aria-label="tag animals" href="#">
-//                     <span class="tag">#${res[i][j]}</span>
-//                     </a>
-//                 `
-//                 tags[i].innerHTML = affichageTags
-//             }
-//             affichageTags = ''
-//         }
-//     }
-
-
-// // FILTRER LES PHOTOGRAPHES AVEC TAG DANS NAVIGATION
-// tagFilter.forEach(item => {
-//     item.addEventListener('click', e => {
-//        console.log(tags);
-//     })
-// })
-
-// requete()
